c4a027d7a0ccb4901b9629480e251b33a34e0d4b
theborgh <borghiborghi@gmail.com>
2024-01-11 18:44:18 +0100

    add sample templates

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def homepage(request):
+    context = {'first_name': 'Jon', 'last_name': 'Doe'}
+    return render(request, 'crm/index.html', context)
+
+def clients(request):
+    clientList = [
+        {'id': 1, 'name': 'Jon Doe', 'occupation': 'carpenter', 'email': 'johndoe@hotmail.com'},
+        {'id': 2, 'name': 'Jane Doe', 'occupation': 'plumber', 'email': 'jane@gmail.de'},
+        {'id': 3, 'name': 'John Smith', 'occupation': 'electrician', 'email': 'john@libero.it'},
+        {'id': 4, 'name': 'Jane Smith', 'occupation': 'carpenter', 'email': 'fidgety@smash.com'},
+    ]
+
+    context = {'clients': clientList}
+
+    return render(request, 'crm/clients.html', context)
+
+def register(request):
+    return HttpResponse('This is the registration page')
