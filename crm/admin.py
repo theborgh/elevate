398689948b159c6995232b7719dd27731bf199a4
theborgh <borghiborghi@gmail.com>
2024-01-11 19:22:13 +0100

    create second model with foreign key

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Task, Review
+
+admin.site.register(Task)
+admin.site.register(Review)
--- crm/models.py
+++ crm/models.py
@@ -6,3 +6,10 @@ class Task(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Review(models.Model):
+    reviewer_name = models.CharField(max_length=50)
+    review_title = models.CharField(max_length=100)
+    review_text = models.TextField(max_length=1000)
+
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
